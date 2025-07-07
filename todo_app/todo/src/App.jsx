0375b85a547c3f4a6c0bc9934e15fa470362970f
Deepansh kushwaha <deepanshkushwaha9@gmail.com>
2025-07-08 01:54:13 +0530

    added a feature of ading tasks in todo app

--- todo_app/todo/src/App.jsx
+++ todo_app/todo/src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputvalue, setInputvalue] = useState("");
+  const [todolist, setTodolist] = useState([]);
 
+  const handleChange =(event)=>{
+    setInputvalue(event.target.value);
+  }
+
+  const addTask = () => {
+    setTodolist([...todolist, inputvalue]);
+    setInputvalue("");
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <><div>
+
+      <h1 >BASIC REACT TODO APP</h1>
+      <label htmlFor="inputfield"></label>
+      <input type="text" name='inputfield' onChange={handleChange} value={inputvalue}/>
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {todolist.map((item, index) => {
+          return <li key={index}>{item}</li>
+        })}
+      </ul>
+    </div>
     </>
   )
 }
